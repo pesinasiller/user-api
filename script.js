5fd8dbcd4fecd8d4c4d97dc50e2dda1c6ff9ab6e
Mahdia Noori <mahdianoori14@gmail.com>
2025-07-18 16:11:36 +0430

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// server.mjs
+const { createServer } = require('http');
 const users = [
     {
         name: "carlos",
@@ -19,5 +22,18 @@ const user = users.find((user) => {
     return user.name === 'carlos'
 });
 
-console.log({user});
-console.log("Hey");
+
+
+const server = createServer((req, res) => {
+res.writeHead(200, { 'Content-Type': 'application/json' });
+res.end({users})
+//res.writeHead(200, { 'Content-Type': 'text/plain' });
+  //res.end('Lets Code!\n');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on okay, 127.0.0.1:3000');
+});
+
+// run with `node server.mjs`
